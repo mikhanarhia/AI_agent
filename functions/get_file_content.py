70fa7b67a2f014db93558fbeea487738a1d68af0
mikhanarhia <raihan.hakim2000@gmail.com>
2026-07-28 10:10:42 +0700

    make get file content functions and the test file

---
+++ functions/get_file_content.py
@@ -0,0 +1,24 @@
+import os
+from config import MAX_CHARS
+
+def get_file_content(working_directory: str, file_path: str) -> str:
+    try:
+        abs_working_dir = os.path.abspath(working_directory)
+        target_file = os.path.normpath(os.path.join(abs_working_dir, file_path))
+        if os.path.commonpath([abs_working_dir, target_file]) != abs_working_dir:
+            return f'Error: Cannot list "{file_path}" as it is outside the permitted working directory'
+        if not os.path.isfile(target_file):
+            return f'Error: File not found or is not a regular file: "{file_path}"'
+
+        with open(target_file, "r") as f:
+            # Read first 100 characters only
+            content = f.read(MAX_CHARS)
+
+            # After reading the first MAX_CHARS...
+            if f.read(1):
+                content += f'[...File "{file_path}" truncated at {MAX_CHARS} characters]'
+
+        return content
+
+    except Exception as e:
+        return f"Error: {e}"
