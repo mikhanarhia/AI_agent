520095fc0e6e97faa395dc26a50c3da2b56ba53b
mikhanarhia <raihan.hakim2000@gmail.com>
2026-07-30 06:43:13 +0700

    edit call_function to enable the AI to call the function, not finished

--- call_function.py
+++ call_function.py
@@ -1,8 +1,9 @@
-from functions.get_files_info import schema_get_files_info
-from functions.get_file_content import schema_get_file_content
-from functions.run_python_file import schema_run_python_file
-from functions.write_file import schema_write_file
-
+from functions.get_files_info import get_files_info, schema_get_files_info
+from functions.get_file_content import get_file_content, schema_get_file_content
+from functions.run_python_file import run_python_file, schema_run_python_file
+from functions.write_file import schema_write_file, write_file
+import json
+from collections.abc import Callable
 
 available_functions = [
     schema_get_files_info,
@@ -10,3 +11,24 @@ available_functions = [
     schema_run_python_file,
     schema_write_file,
 ]
+
+def call_function(tool_call, verbose: bool = False) -> dict:
+    function_name = tool_call.function.name
+    function_args = json.loads(tool_call.function.arguments or "{}")
+    if verbose:
+        print(f" - Calling function: {function_name}({function_args})")
+    print(f" - Calling function: {function_name}")
+
+    function_map: dict[str, Callable[..., str]] = {
+        "get_file_content": get_file_content,
+        "get_files_info": get_files_info,
+        "run_python_file": run_python_file,
+        "write_file": write_file,
+    }
+
+    if function_name not in function_map:
+        return {
+            "role": "tool",
+            "tool_call_id": tool_call.id,
+            "content": f"Error: Unknown function: {function_name}",
+        }
